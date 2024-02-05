6f885c56c3e43348d393a00f4d63a9e70090b8c1
SilenceShine <xuansy0127@gmail.com>
2024-02-05 15:46:29 +0800

    feat: add mybatis recursion function.

--- shine-framework-spring/shine-framework-spring-orm/src/main/java/io/github/SilenceShine/shine/spring/orm/page/MybatisPageUtil.java
+++ shine-framework-spring/shine-framework-spring-orm/src/main/java/io/github/SilenceShine/shine/spring/orm/page/MybatisPageUtil.java
@@ -4,12 +4,19 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.metadata.OrderItem;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.github.SilenceShine.shine.core.dto.PageModel;
+import io.github.SilenceShine.shine.util.log.LogUtil;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 /**
  * @author SilenceShine
  * @github <a href="https://github.com/SilenceShine">SilenceShine</a>
  * @since 1.0
  */
+@Slf4j
 public class MybatisPageUtil extends PageUtil {
 
     public static <M> IPage<M> pageSingle() {
@@ -29,6 +36,15 @@ public class MybatisPageUtil extends PageUtil {
             }
         });
         return page;
+    }
+
+    public static <T> void recursion(IPage<T> page, Function<IPage<T>, IPage<T>> function, Consumer<List<T>> consumer) {
+        LogUtil.debug(log, "mybatis current:{} size:{} total:{}", page.getCurrent(), page.getSize(), page.getTotal());
+        IPage<T> iPage = function.apply(page);
+        consumer.accept(iPage.getRecords());
+        if (iPage.getCurrent() >= iPage.getPages()) return;
+        page.setCurrent(iPage.getCurrent() + 1);
+        recursion(iPage, function, consumer);
     }
 
 }
--- shine-framework-spring/shine-framework-spring-orm/src/main/java/io/github/SilenceShine/shine/spring/orm/page/PageUtil.java
+++ shine-framework-spring/shine-framework-spring-orm/src/main/java/io/github/SilenceShine/shine/spring/orm/page/PageUtil.java
@@ -2,6 +2,7 @@ package io.github.SilenceShine.shine.spring.orm.page;
 
 import io.github.SilenceShine.shine.core.dto.PageModel;
 import io.github.SilenceShine.shine.util.core.StrUtils;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 import java.util.function.BiConsumer;
@@ -11,6 +12,7 @@ import java.util.function.BiConsumer;
  * @github <a href="https://github.com/SilenceShine">SilenceShine</a>
  * @since 1.0
  */
+@Slf4j
 public class PageUtil {
 
     public static void sort(PageModel<?> model, BiConsumer<Boolean, List<String>> consumer) {
